0cd270a9e46be92a6e2d7411e965bc811b8fb912
warriordog <warriordog@github.com>
2018-09-11 22:23:17 -0400

    Add utility variants of ExecQuery

--- src/java/net/acomputerdog/jwmi/wbem/WbemServices.java
+++ src/java/net/acomputerdog/jwmi/wbem/WbemServices.java
@@ -5,8 +5,11 @@ import com.sun.jna.platform.win32.WTypes.BSTR;
 import com.sun.jna.platform.win32.WinNT.HRESULT;
 import com.sun.jna.ptr.PointerByReference;
 import net.acomputerdog.jwmi.WMIException;
-import net.acomputerdog.jwmi.nat.WMIWrapper;
 import net.acomputerdog.jwmi.nat.ReleasableNativeObject;
+import net.acomputerdog.jwmi.nat.WMIWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WbemServices extends ReleasableNativeObject {
 
@@ -28,6 +31,34 @@ public class WbemServices extends ReleasableNativeObject {
             return new EnumWbemClassObject(clsObjEnum.getValue());
         } else {
             throw new WMIException("WMI error occurred: " + result.toString(), result);
+        }
+    }
+
+    public List<WbemClassObject> execQueryBuffered(String query) {
+        try (EnumWbemClassObject clsObjEnum = execQuery(query)) {
+            List<WbemClassObject> clsObjList = new ArrayList<>();
+
+            try {
+                while (clsObjEnum.hasNext()) {
+                    clsObjList.add(clsObjEnum.next());
+                }
+
+                return clsObjList;
+            } catch (Throwable t) {
+                clsObjList.forEach(WbemClassObject::release);
+
+                throw t;
+            }
+        }
+    }
+
+    public WbemClassObject execQuerySingle(String query) {
+        try (EnumWbemClassObject clsObjEnum = execQuery(query)) {
+            if (clsObjEnum.hasNext()) {
+                return clsObjEnum.next();
+            } else {
+                return null;
+            }
         }
     }
 }
